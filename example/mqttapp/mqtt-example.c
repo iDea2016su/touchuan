71bd8e3c29928d10fe60a62492e061ba5eb1fe61
huka <huka.cn@gmail.com>
2018-08-07 15:36:59 +0800

    finish

--- example/mqttapp/mqtt-example.c
+++ example/mqttapp/mqtt-example.c
@@ -8,16 +8,18 @@
 
 #include "iot_import.h"
 #include "iot_export.h"
+#include "iot_export_mqtt.h"
 #include "aos/log.h"
 #include "aos/yloop.h"
 #include "aos/network.h"
 #include <netmgr.h>
 #include <aos/kernel.h>
+#include <k_err.h>
 #include <netmgr.h>
 #include <aos/cli.h>
 #include <aos/cloud.h>
-#include "iot_export_mqtt.h"
-#include "mqtt_instance.h"
+
+#include "soc_init.h"
 
 #ifdef AOS_ATCMD
 #include <atparser.h>
@@ -41,8 +43,44 @@ typedef struct {
 #define TOPIC_ERROR             "/"PRODUCT_KEY"/"DEVICE_NAME"/update/error"
 #define TOPIC_GET               "/"PRODUCT_KEY"/"DEVICE_NAME"/get"
 
+#define ALINK_BODY_FORMAT         "{\"id\":\"%d\",\"version\":\"1.0\",\"method\":\"%s\",\"params\":%s}"
+
+#define RAW_TOPIC_PROP_UP         "/sys/"PRODUCT_KEY"/"DEVICE_NAME"/thing/model/up_raw"
+#define RAW_TOPIC_PROP_DOWN       "/sys/"PRODUCT_KEY"/"DEVICE_NAME"/thing/model/down_raw"
+
+#define ALINK_METHOD_PROP_POST    "thing.event.property.post"
+#define ALINK_METHOD_EVENT_POST    "thing.event.Alarm.post"
+
 #define MSG_LEN_MAX             (2048)
 
+#define FRE_KEY_ADDR  "led_fre"
+#define FRE_BUF_LEN   (64)
+static int  led_fre = 3; 
+
+#define TEMP_KEY_ADDR  "temp_val"
+#define TEMP_BUF_LEN   (64)
+static int  temp_val = 320; 
+static int  alarm_status = 0; 
+static int led_fre_upload_status =1;
+
+static int alarm_clear = 0;
+
+void set_led_fre(int p_fre);
+int get_led_fre(void);
+
+static void app_delayed_action(void *arg)
+{
+    static int count =0;
+    int fre_count = get_led_fre();
+    count++;
+    if(count>=(10/fre_count))
+    {
+
+         hal_gpio_output_toggle(&brd_gpio_table[8]);
+        count = 0;
+    }
+    aos_post_delayed_action(100, app_delayed_action, NULL);
+}
 int cnt = 0;
 static int is_subscribed = 0;
 
@@ -50,8 +88,36 @@ static int is_subscribed = 0;
 static int sub_counter = 0;
 static int pub_counter = 0;
 #endif
-char msg_pub[128];
+char msg_pub[512];
+static int fd_temp  = -1;
 
+static int sensor_all_open(void)
+{
+    int fd = -1;
+    fd = aos_open(dev_temp_path, O_RDWR);
+    if (fd < 0) {
+        printf("Error: aos_open return %d.\n", fd);
+        return -1;
+    }
+    fd_temp = fd;
+    printf("temp socket  %d\n", fd);
+    return 0;
+}
+
+static int get_temp_data(int *x)
+{
+    temperature_data_t temp = {0};
+    ssize_t size = 0;
+    size = aos_read(fd_temp, &temp, sizeof(temp));
+    if (size != sizeof(temp)) {
+        printf("aos_read return error.\n");
+        return -1;
+    }
+    *x = temp.t;
+    return 0;
+}
+
+static int t_count = 20;
 static void ota_init(void *pclient);
 int mqtt_client_example(void);
 static void wifi_service_event(input_event_t *event, void *priv_data)
@@ -76,56 +142,73 @@ static void mqtt_sub_callback(char *topic, int topic_len, void *payload, int pay
         topic_len);
     LOG("Payload: '%.*s' (Length: %d)",
         payload_len,
-        (char *)payload,
+        (unsigned char *)payload,
         payload_len);
     LOG("----");
-
-#ifdef MQTT_PRESS_TEST
-    sub_counter++;
-    int rc = mqtt_publish(TOPIC_UPDATE, IOTX_MQTT_QOS1, payload, payload_len);
-    if (rc < 0) {
-        LOG("IOT_MQTT_Publish fail, ret=%d", rc);
-    } else {
-        pub_counter++;
+    unsigned char receive_buf[30]={0};
+    for(int i=0;i<payload_len;i++)
+    {
+        receive_buf[i] = ((unsigned char *)payload)[i];
     }
-    LOG("RECV=%d, SEND=%d", sub_counter, pub_counter);
-#endif MQTT_PRESS_TEST
+    int rec_temp = ((int)receive_buf[5])*10+(int)receive_buf[6];
+    int rec_fre = ((int)receive_buf[7])*10+(int)receive_buf[8];
+    printf("rec_temp %d\n",rec_temp);
+    printf("rec_fre %d\n",rec_fre);
+    if(rec_fre>0)
+    {
+        set_led_fre(rec_fre);
+    }  
 }
 
-
-
-/*
- * Subscribe the topic: IOT_MQTT_Subscribe(pclient, TOPIC_DATA, IOTX_MQTT_QOS1, _demo_message_arrive, NULL);
- * Publish the topic: IOT_MQTT_Publish(pclient, TOPIC_DATA, &topic_msg);
- */
 static void mqtt_work(void *parms)
 {
 
     int rc = -1;
-
-    if (is_subscribed == 0) {
+    if (is_subscribed == 0) 
+    {
         /* Subscribe the specific topic */
-        rc = mqtt_subscribe(TOPIC_GET, mqtt_sub_callback, NULL);
+        rc = mqtt_subscribe(RAW_TOPIC_PROP_DOWN, mqtt_sub_callback, NULL);
         if (rc < 0) {
             // IOT_MQTT_Destroy(&pclient);
             LOG("IOT_MQTT_Subscribe() failed, rc = %d", rc);
         }
         is_subscribed = 1;
-        aos_schedule_call(ota_init, NULL);
     }
-#ifndef MQTT_PRESS_TEST
-    else {
+    else
+    {
         /* Generate topic message */
-        int msg_len = snprintf(msg_pub, sizeof(msg_pub), "{\"attr_name\":\"temperature\", \"attr_value\":\"%d\"}", cnt);
-        if (msg_len < 0) {
-            LOG("Error occur! Exit program");
+        memset(msg_pub, 0, sizeof(msg_pub));
+        int temperature =0;
+        get_temp_data(&temperature);        
+        int frequency = get_led_fre();
+        int temp = temperature/10; 
+        int frequency_h = frequency/256;
+        int frequency_l = frequency%256;
+        int temp_h = temp/256;
+        int temp_l = temp%256;
+        LOG("temperature : %d\n",temp);
+        LOG("frequency :   %d\n",frequency);
+        {
+            unsigned char p_buf[10]={0};
+            p_buf[0]=0;
+            p_buf[1]=cnt/1000;
+            p_buf[2]=(cnt/100)%10;
+            p_buf[3]=(cnt/10)%10;
+            p_buf[4]=cnt%10;
+            p_buf[5]=temp_h;
+            p_buf[6]=temp_l;
+            p_buf[7]=frequency_h;
+            p_buf[8]=frequency_l;
+            rc = mqtt_publish(RAW_TOPIC_PROP_UP, IOTX_MQTT_QOS1, p_buf, 9);
+            if (rc < 0) LOG("error occur when publish");
+            LOG("message : ");
+            for(int i=0;i<9;i++)
+            {
+                printf("%x",p_buf[i]);
+            }
+            printf("\n");
         }
-        rc = mqtt_publish(TOPIC_UPDATE, IOTX_MQTT_QOS1, msg_pub, msg_len);
-        if (rc < 0) {
-            LOG("error occur when publish");
-        }
-
-        LOG("packet-id=%u, publish topic msg=%s", (uint32_t)rc, msg_pub);
+        LOG("system is running %d\n",cnt);
     }
     cnt++;
     if (cnt < 200) {
@@ -138,7 +221,6 @@ static void mqtt_work(void *parms)
         is_subscribed = 0;
         cnt = 0;
     }
-#endif
 }
 
 
@@ -149,7 +231,6 @@ static void mqtt_service_event(input_event_t *event, void *priv_data)
     if (event->type != EV_SYS) {
         return;
     }
-
     if (event->code != CODE_SYS_ON_MQTT_READ) {
         return;
     }
@@ -164,7 +245,6 @@ static int smartled_event_handler(int event_type, void *ctx)
         default:
             break;
     }
-
     return 0;
 }
 
@@ -216,6 +296,8 @@ static void at_uart_configure(uart_dev_t *u)
 }
 #endif
 
+
+
 int application_start(int argc, char *argv[])
 {
 #if AOS_ATCMD
@@ -232,13 +314,15 @@ int application_start(int argc, char *argv[])
 #endif
 
     aos_set_log_level(AOS_LL_DEBUG);
-
+    sensor_all_open();
     aos_register_event_filter(EV_WIFI, wifi_service_event, NULL);
 
     netmgr_init();
     netmgr_start(false);
 
     aos_cli_register_command(&mqttcmd);
+
+    aos_post_delayed_action(100, app_delayed_action, NULL);
 
     aos_loop_run();
     return 0;
@@ -248,3 +332,14 @@ static void ota_init(void *P)
 {
     aos_post_event(EV_SYS, CODE_SYS_ON_START_FOTA, 0u);
 }
+
+void set_led_fre(int p_fre)
+{
+    led_fre = p_fre;
+}
+
+int get_led_fre(void)
+{
+    return led_fre;
+}
+
